af4bd458946fa905f73c3dc7ad668f7aed9227c3
Yaroslav Oslam <ggStrider1@gmail.com>
2025-08-28 23:31:29 +0300

    clean-up: removed now unnecesesary tags

--- Assets/_Project/_Scripts/Internal/StaticKeys.cs
+++ Assets/_Project/_Scripts/Internal/StaticKeys.cs
@@ -1,9 +1,5 @@
 ﻿public static class StaticKeys
 {
-    // TODO: Refactor to remove these enemy tags at all
-    public const string ENEMY_TAG = "Enemy";
-    public const string FAKE_ENEMY_TAG = "FakeEnemy";
     public const string PLAYER_TAG = "Player";
-
     public const string PROJECT_NAME = "ggStrider";
 }
