f6209587004744e33681773200135ea09a69d04b
Yaroslav Oslam <ggStrider1@gmail.com>
2025-08-30 13:11:51 +0300

    added sound when player damaged

--- Assets/_Project/_Scripts/Internal/Core/Audio/AudioEffectsManager.cs
+++ Assets/_Project/_Scripts/Internal/Core/Audio/AudioEffectsManager.cs
@@ -1,4 +1,5 @@
 ﻿using Internal.Core.Signals;
+using Internal.Gameplay.EntitiesShared;
 using UnityEngine;
 using Zenject;
 
@@ -10,28 +11,35 @@ namespace Audio
 
         [Space] [SerializeField] private AudioPlayerAdvanced _onEnemyHitEffect;
         [SerializeField] private AudioPlayerAdvanced _onPlayerShootEffect;
+        [SerializeField] private AudioPlayerAdvanced _onPlayerDamagedEffect;
 
         private SignalBus _signalBus;
+        private PlayerHealth _playerHealth;
 
         [Inject]
-        private void Construct(SignalBus signalBus)
+        private void Construct(SignalBus signalBus, PlayerHealth playerHealth)
         {
             _signalBus = signalBus;
+            _playerHealth = playerHealth;
         }
 
         private void OnEnable()
         {
             SubscribeOnSignalBusEvents();
+            
+            // TODO: Refactor Into Signal Bus
+            _playerHealth.OnDamageTaken += PlayPlayerDamagedSound;
         }
 
         private void OnDisable()
         {
             UnsubscribeOnSignalBusEvents();
+            _playerHealth.OnDamageTaken -= PlayPlayerDamagedSound;
         }
 
         private void SubscribeOnSignalBusEvents()
         {
-            _signalBus.Subscribe<EnemyHitInPlayerSignal>(PlayEnemyHitInPlayerSound);
+            // _signalBus.Subscribe<EnemyHitInPlayerSignal>(PlayEnemyHitInPlayerSound);
             _signalBus.Subscribe<PlayerShootSignal>(PlayPlayerShootSound);
         }
 
@@ -49,6 +57,11 @@ namespace Audio
         private void PlayPlayerShootSound()
         {
             _onPlayerShootEffect.PlayShotOfRandomSound();
+        }
+        
+        private void PlayPlayerDamagedSound(int obj)
+        {
+            _onPlayerDamagedEffect.PlayShotOfRandomSound();
         }
     }
 }
