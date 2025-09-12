d1beb563a86fc928a532ff63bdbb11ddd9b5b5c7
Yaroslav Oslam <ggStrider1@gmail.com>
2025-09-12 19:59:56 +0300

    feature: bass shake

--- Assets/_Project/_Scripts/Internal/Gameplay/Managers/CameraManager.cs
+++ Assets/_Project/_Scripts/Internal/Gameplay/Managers/CameraManager.cs
@@ -11,12 +11,23 @@ namespace Internal.Gameplay.Managers
     {
         [SerializeField] private float _shakeDuration = 0.1f;
         [SerializeField] private float _shakeStrength = 0.25f;
-        
+
+        [Space, Header("Bass Shake")] 
+        [SerializeField] private Transform _bassShakeTransform;
+        [SerializeField] private float _pulseStrength = 0.2f;
+        [SerializeField] private float _pulseDuration = 0.1f;
+        [SerializeField] private int _punchVibrato = 5;
+        [SerializeField] private float _zoomStrength = 0.2f;
+        [SerializeField] private float _zoomDuration = 0.1f;
+
+        private float _defaultOrthoSize;
+        private Vector3 _defaultBassShakePosition;
+
         /// <summary>
         /// Injects by <see cref="Installers.PlayerInstaller"/>
         /// </summary>
-        [Header("Injects by Zenject")]
-        [SerializeField, ReadOnly] private Camera _mainCamera;
+        [Header("Injects by Zenject")] [SerializeField, ReadOnly]
+        private Camera _mainCamera;
 
         private Vector3 _startCameraPosition;
         private SignalBus _signalBus;
@@ -34,6 +45,9 @@ namespace Internal.Gameplay.Managers
         private void Awake()
         {
             _startCameraPosition = _mainCamera.transform.position;
+
+            _defaultOrthoSize = _mainCamera.orthographicSize;
+            _defaultBassShakePosition = _bassShakeTransform.localPosition;
         }
 
         private void OnEnable()
@@ -48,15 +62,34 @@ namespace Internal.Gameplay.Managers
         {
             _signalBus.TryUnsubscribe<RealEnemyKilledSignal>(ShakeCamera);
             _signalBus.TryUnsubscribe<FakeEnemyKilledSignal>(ShakeCamera);
-            
+
             _playerHealth.OnDamageTaken -= _ => ShakeCamera();
         }
 
+        [Button]
         private void ShakeCamera()
         {
             _mainCamera.DOKill();
             _mainCamera.DOShakePosition(_shakeDuration, _shakeStrength)
                 .OnComplete(() => _mainCamera.transform.position = _startCameraPosition);
+        }
+
+        [Button]
+        private void BassShakeCamera()
+        {
+            _mainCamera.DOKill();
+            _bassShakeTransform.DOKill();
+
+            _mainCamera.DOOrthoSize(_defaultOrthoSize - _zoomStrength, _zoomDuration)
+                .SetLoops(2, LoopType.Yoyo)
+                .OnComplete(() => _mainCamera.orthographicSize = _defaultOrthoSize);
+
+            _bassShakeTransform.DOPunchPosition(
+                new Vector3(0, -_pulseStrength, 0),
+                _pulseDuration,
+                _punchVibrato,
+                0.5f
+            ).OnComplete(() => _bassShakeTransform.localPosition = _defaultBassShakePosition);
         }
     }
 }
