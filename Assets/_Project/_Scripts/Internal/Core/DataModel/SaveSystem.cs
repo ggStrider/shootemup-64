94766a45f80acdb4d74459f0aca630e1f075e572
Yaroslav Oslam <ggStrider1@gmail.com>
2025-09-12 14:09:05 +0300

    feature: ability to delete local save

--- Assets/_Project/_Scripts/Internal/Core/DataModel/SaveSystem.cs
+++ Assets/_Project/_Scripts/Internal/Core/DataModel/SaveSystem.cs
@@ -1,5 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using UnityEditor;
+
+#if UNITY_EDITOR
 using UnityEngine;
+#endif
 
 namespace Internal.Core.DataModel
 {
@@ -17,12 +22,42 @@ namespace Internal.Core.DataModel
         {
             if (!File.Exists(SavePath))
             {
-                Debug.unityLogger.Log($"[{nameof(SaveSystem)}] No Save Found!");
+                Debug.unityLogger.Log($"[{nameof(SaveSystem)}] No Save Found! Creating new...");
                 return new();
             }
 
             var jsonText = File.ReadAllText(SavePath);
             return JsonUtility.FromJson<PlayerData>(jsonText);
         }
+        
+        public static bool TryDeleteSave()
+        {
+            if (!File.Exists(SavePath))
+            {
+                Debug.Log($"<color=green>[{nameof(SaveSystem)}] Save doesn't exist!</color>");
+                return true;
+            }
+
+            try
+            {
+                File.Delete(SavePath);
+                
+                Debug.Log($"<color=green>[{nameof(SaveSystem)}] Successfully deleted local save!</color>");
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Debug.LogError($"[{nameof(SaveSystem)}] Failed to delete save file: {exception}");
+                return false;
+            }
+        }
+
+#if UNITY_EDITOR
+        [MenuItem(StaticKeys.PROJECT_NAME + "/Fast Utilities/Delete Save")]
+        public static void DeleteSave_Editor()
+        {
+            TryDeleteSave();
+        }
+#endif
     }
 }
