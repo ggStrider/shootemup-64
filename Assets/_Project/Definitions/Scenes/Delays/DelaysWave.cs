e3ddcb192243bee03652e732d9749798297a3d04
Yaroslav Oslam <ggStrider1@gmail.com>
2025-09-15 13:30:06 +0300

    feature: ability to copy delays from bg; updated SO

--- Assets/_Project/Definitions/Scenes/Delays/DelaysWave.cs
+++ Assets/_Project/Definitions/Scenes/Delays/DelaysWave.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections.Generic;
+using Definitions.Scenes.Delays.BackgroundChanger;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace Definitions.Scenes.CameraBassShake
 {
-    [CreateAssetMenu(fileName = "New Delays Wave", menuName = 
+    [CreateAssetMenu(fileName = "New Delays Wave", menuName =
         StaticKeys.PROJECT_NAME + "/Definitions/Delays/Any Delays Waves")]
     public class DelaysWave : ScriptableObject
     {
-        [field:SerializeField] public List<float> Delays { get; private set; } = new();
+        [field: SerializeField] public List<float> Delays { get; private set; } = new();
 
         public static implicit operator List<float>(DelaysWave waves)
         {
@@ -16,5 +18,20 @@ namespace Definitions.Scenes.CameraBassShake
 
         public int Count => Delays.Count;
         public float this[int index] => Delays[index];
+
+#if UNITY_EDITOR
+        [SerializeField] private BackgroundChangeDelays _delays;
+        
+        [Button]
+        public void CopyFromBgChanger()
+        {
+            Delays = new();
+            foreach (var t in _delays.DelaysWith2T)
+            {
+                var delay = t.Delay;
+                Delays.Add(delay);
+            }
+        }
+#endif
     }
 }
